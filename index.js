2a3fe007e414fd2eccb8207671736a350b0c7b36
souravMitra02 <inf.souravmitra@gmail.com>
2025-05-24 17:09:58 +0600

    addedto port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
+// port
 const port = process.env.PORT || 3000;
 
 app.use(cors());
