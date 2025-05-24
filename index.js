79192a15d81accf21a68f5266bb47527fd8c1f60
souravMitra02 <inf.souravmitra@gmail.com>
2025-05-24 17:10:58 +0600

    added mongodb uri

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// mongobd uri
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssk8yog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
