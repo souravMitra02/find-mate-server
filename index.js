20697c0d26a420df571b7dc13d85c95e1fb60d8e
souravMitra02 <inf.souravmitra@gmail.com>
2025-06-27 09:55:28 +0600

    added server data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// port
+// PORT
 const port = process.env.PORT || 3000;
 
 app.use(cors());
